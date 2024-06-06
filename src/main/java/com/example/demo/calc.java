8935bd58ae43a29e8811170900df7306984154b1
Kento-Ikushima <rad.okawari171@gmail.com>
2024-06-06 23:48:38 +0900

    Your commit message

---
+++ src/main/java/com/example/demo/calc.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+public class calc {
+
+	int x=100;
+	int y=200;
+	
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		arithmeticSample am =new arithmeticSamle();
+		
+		System.out.println("足し算をします\n");
+		int ans = am.add(x,y);
+		System.out.println("答えは" + ans + "です");
+
+	}
+
+}
